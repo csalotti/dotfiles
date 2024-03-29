de85f9cb8e64e273ee3a5163281f8d0384d01839
Christopher Salotti <christopher.salotti@gmail.com>
2024-03-29 13:59:35 +0100

    feat(fish): Add fzf seatch on dev only

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,7 @@
 alias python python3
 # Go to directory from the searched file
 alias sd "cd ~ && cd (find * -type d | fzf)"
+alias sdev "cd ~/Dev  && cd (find * -type d | fzf)"
 
 # Variables
 # This prevents me from installing packages with pip without being
