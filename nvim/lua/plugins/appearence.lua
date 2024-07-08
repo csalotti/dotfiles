b9086376ec53aaa4965284d36ca452ec1ea42dc6
Christopher Salotti <christopher.salotti@gmail.com>
2024-07-08 09:50:30 +0200

    nvim: Clean and use default theme

--- nvim/lua/plugins/appearence.lua
+++ nvim/lua/plugins/appearence.lua
@@ -1,34 +1,12 @@
 return {
   {
-    -- Theme
-    "EdenEast/nightfox.nvim",
-    config = function()
-      vim.cmd.colorscheme 'carbonfox'
-    end,
-    opts = {
-      options = {
-        colorblind = {
-          enable = true,         -- Enable colorblind support
-          simulate_only = false, -- Only show simulated colorblind colors and not diff shifted
-          severity = {
-            protan = 0,          -- Severity [0,1] for protan (red)
-            deutan = 1,          -- Severity [0,1] for deutan (green)
-            tritan = 0,          -- Severity [0,1] for tritan (blue)
-          }
-        }
-      }
-    }
-  },
-  {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
     -- See `:help lualine.txt`
     opts = {
       options = {
-        icons_enabled = false,
+        icons_enabled = true,
         theme = 'auto',
-        component_separators = '|',
-        section_separators = '',
       },
     },
   },
