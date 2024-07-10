4c87d48e217bf55bb8a4e6a35d5c017f7870095c
Christopher Salotti <christopher.salotti@gmail.com>
2024-07-10 14:17:15 +0200

    nvim: switch to basedpyright with ruff

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -95,16 +95,13 @@ return {
       -- zig
       zls = {},
       -- python
-      pyright = {
-        pyright = {
+      basedpyright = {
+        basedpyright = {
           -- Using Ruff's import organizer
           disableOrganizeImports = true,
-        },
-        python = {
           analysis = {
-            -- Ignore all files for analysis to exclusively use Ruff for linting
+            typeCheckingMode = "basic"
             -- ignore = { '*' },
-            typeCheckingMode = 'off',
           },
         },
       },
