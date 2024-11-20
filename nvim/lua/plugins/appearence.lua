4c586cb4783fa516add90cc021aa3e519b03b46f
Christopher Salotti <christopher.salotti@gmail.com>
2024-11-20 13:26:56 +0100

    nvim: add themes, lint and markdown peek

--- nvim/lua/plugins/appearence.lua
+++ nvim/lua/plugins/appearence.lua
@@ -10,4 +10,19 @@ return {
       },
     },
   },
+  {
+    "folke/tokyonight.nvim",
+    style = "night",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    init = function()
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      vim.cmd.colorscheme 'kanagawa'
+    end
+  },
 }
