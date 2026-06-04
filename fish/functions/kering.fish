976c1faf17daefd5efc5eff0e01893ebbfaabf93
Christopher Salotti <christopher.salotti@gmail.com>
2026-06-04 15:27:12 +0200

    feat(fish): sync fish configs

--- fish/functions/kering.fish
+++ fish/functions/kering.fish
@@ -3,11 +3,10 @@ function set_terraformer_sa --on-variable PWD --description "Set proper service 
     # Get top level repository name (if any)
     set REPO_ROOT (git rev-parse --show-toplevel 2>/dev/null)
 
-    set PROD_FOLDERS prod github transversal-prod github_infra shared_dbt_macros tf_workaround_roles airbyte-monitoring-prod
-    set DEV_FOLDERS dev transversal-dev airbyte-monitoring-dev
+    set PROD_FOLDERS dummy2 dummy4 dummy6 dummy8 dummy10 prod dev github transversal-prod github_infra shared_dbt_macros tf_workaround_roles
+    set DEV_FOLDERS dummy1 dummy3 dummy5 dummy7 dummy9 transversal-dev
 
-    # Limit to kering infra repo
-    if test -n "$REPO_ROOT"; and test (basename "$REPO_ROOT") = "data-platform-infrastructure"
+    if test -n "$REPO_ROOT"; and test (basename "$REPO_ROOT") = data-platform-infrastructure
         set CURRENT_FOLDER (basename $PWD)
         if contains "$CURRENT_FOLDER" $PROD_FOLDERS
             echo "[GCP] impersonate terraform-prod"
@@ -19,11 +18,11 @@ function set_terraformer_sa --on-variable PWD --description "Set proper service 
             # Remove variable when not any appropriate foler
             set -e GOOGLE_IMPERSONATE_SERVICE_ACCOUNT
         end
-    else if test -n "$REPO_ROOT"; and test (basename "$REPO_ROOT") = "data-platform-infrastructure_exploration"
-	echo "[GCP] impersonate terraform-prod"
-	set -gx GOOGLE_IMPERSONATE_SERVICE_ACCOUNT terraformer@ke-data-transversal-prod.iam.gserviceaccount.com
+    else if test -n "$REPO_ROOT"; and test (basename "$REPO_ROOT") = data-platform-infrastructure_exploration
+        echo "[GCP] impersonate terraform-prod"
+        set -gx GOOGLE_IMPERSONATE_SERVICE_ACCOUNT terraformer@ke-data-transversal-prod.iam.gserviceaccount.com
     else if test -n "$GOOGLE_IMPERSONATE_SERVICE_ACCOUNT"
-	# Remove variable when not any appropriate foler
-	set -e GOOGLE_IMPERSONATE_SERVICE_ACCOUNT
+        # Remove variable when not any appropriate foler
+        set -e GOOGLE_IMPERSONATE_SERVICE_ACCOUNT
     end
 end
